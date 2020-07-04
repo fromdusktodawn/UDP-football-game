4af60f50d5fd68cd146940efc7cc522b5ba802c9
fromdusktodawn <2563546603@qq.com>
2020-07-04 18:49:26 +0800

    updata

--- server/server.c
+++ server/server.c
@@ -15,6 +15,7 @@ int data_port;
 int port = 0;
 struct Bpoint ball;
 struct BallStatus ball_status;
+struct Score score;
 
 extern struct Map court;
 extern WINDOW *Football, *Message, *Help, *Score, *Write;
